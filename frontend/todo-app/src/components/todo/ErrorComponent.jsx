7f822efe5c3bd487f23c24bb7aac4f0b49604854
Liz Lee <lizabethmarielee@gmail.com>
2021-08-24 22:56:15 +0800

    first commit

---
+++ frontend/todo-app/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ErrorComponent() {
+    return <div>An Error Occurred. I don't know what to do! Contact support at 9999999999</div>
+}
+
+export default ErrorComponent
